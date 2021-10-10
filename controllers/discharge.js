d842caea9a38ecd3baedd9b8ca2913b2a7d5c5d4
adarshjp <adarsh.24padmashali@gmail.com>
2021-10-10 20:35:08 +0530

    formatted-prettier

--- controllers/discharge.js
+++ controllers/discharge.js
@@ -1,73 +1,7 @@
-const District = require("../models/district");
 const Hospital = require("../models/hospital");
 const Patient = require("../models/patient");
 const PatientsAdmitted = require("../models/patientadmitted");
 
-exports.discharge = (req, res) => {
-  console.log(req.params);
-  Patient.findOneAndUpdate(
-    { _id: req.params.id },
-    { status: req.params.status },
-    (err, statusUpPatient) => {
-      if (err) console.log(err);
-      else {
-        console.log(statusUpPatient);
-        var type = statusUpPatient.typeofbedreq;
-        Patient.findOneAndUpdate(
-          {
-            districtid: req.user.districtid,
-            status: "Requested",
-            typeofbedreq: type,
-          },
-          { status: "Alloted", allotmenttime: Date.now() },
-          (err, allottedpatient) => {
-            if (err) console.log(err);
-            else if (allottedpatient == null) {
-              var key1 = "avaliablebeds." + type;
-              var key2 = "occupiedbeds." + type;
-              Hospital.findOneAndUpdate(
-                { _id: req.user._id },
-                { $inc: { [key1]: 1, [key2]: -1 } },
-                (err, updatedhosp) => {
-                  if (err) console.log(err);
-                  else {
-                    console.log(updatedhosp);
-                  }
-                }
-              );
-            } else {
-              console.log(allottedpatient);
-              var patadm = new PatientsAdmitted({
-                patientid: allottedpatient._id,
-                hospitalid: req.user._id,
-                districtid: req.user.districtid,
-                typeofbed: allottedpatient.typeofbedreq,
-              });
-              PatientsAdmitted.create(patadm, (err, newpatadm) => {
-                if (err) console.log(err);
-                else console.log(newpatadm);
-              });
-              const msg =
-                "Bed alloted at " +
-                allottedpatient.hospitalid +
-                " Report to hospital within 3 hours.You can find the name in the website";
-              //sendSms('+91'+allottedpatient.phoneno,msg)
-            }
-          }
-        );
-      }
-    }
-  );
-  PatientsAdmitted.findOneAndUpdate(
-    { patientid: req.params.id },
-    { leaving: Date.now() },
-    (err, datepat) => {
-      if (err) console.log(err);
-      else console.log(datepat);
-    }
-  );
-  res.redirect("/viewpatients");
-};
 exports.discharge = (req, res) => {
   console.log(req.params);
   Patient.findOneAndUpdate(
--- models/patientadmitted.js
+++ models/patientadmitted.js
@@ -1,21 +1,21 @@
-const mongoose=require('mongoose')
-const patadmSchema=new mongoose.Schema({
-    patientid:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Patient'
-    },
-    hospitalid:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Hospital'
-    },
-    districtid:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'District'
-    },
-    joining:{
-        type:Date
-    },
-    leaving:Date,
-    typeofbed:String
-})
-module.exports=new mongoose.model('PatientsAdmitted',patadmSchema)
+const mongoose = require("mongoose");
+const patadmSchema = new mongoose.Schema({
+  patientid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+  },
+  hospitalid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hospital",
+  },
+  districtid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "District",
+  },
+  joining: {
+    type: Date,
+  },
+  leaving: Date,
+  typeofbed: String,
+});
+module.exports = new mongoose.model("PatientsAdmitted", patadmSchema);
